5fa0c1fc00876b68a139b857b0f171db93e5d544
Greg <greg.renfro93@gmail.com>
2025-08-25 22:33:11 -0700

    cleaned up ec2 variables

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,33 +1,33 @@
 variable "instance_name" {
-  description = "Name for the EC2 instance"
-  type        = string
+    description = "Name for the EC2 instance"
+    type        = string
 }
 
 variable "instance_type" {
-  description = "Type of EC2 instance"
-  type        = string
-  default     = "t2.micro"
+    description = "Type of EC2 instance"
+    type        = string
+    default     = "t2.micro"
 }
 
 variable "ami_id" {
-  description = "AMI ID for the instance"
-  type        = string
+    description = "AMI ID for the instance"
+    type        = string
 }
 
 variable "subnet_id" {
-  description = "Subnet ID to launch instance in"
-  type        = string
+    description = "Subnet ID to launch instance in"
+    type        = string
 }
 
 variable "vpc_security_group_ids" {
-  description = "Security group IDs"
-  type        = list(string)
+    description = "Security group IDs"
+    type        = list(string)
 }
 
 variable "iam_instance_profile" {
-  description = "IAM instance profile name"
-  type        = string
-  default     = null
+    description = "IAM instance profile name"
+    type        = string
+    default     = null
 }
 
 variable "key_name" {
