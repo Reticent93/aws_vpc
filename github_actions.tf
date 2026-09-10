6ce7cfabf1bad49ae7ff23a3b033dcf6063c0268
Greg <greg.renfro93@gmail.com>
2026-09-10 05:22:18 -0700

    Add GitHub Actions deploy pipeline with OIDC auth

---
+++ github_actions.tf
@@ -0,0 +1,32 @@
+data "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+data "aws_iam_policy_document" "github_assume" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:Reticent93/aws_vpc:ref:refs/heads/main"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "${var.project_name}-github-actions"
+  assume_role_policy = data.aws_iam_policy_document.github_assume.json
+}
