acbd539703edd03d7bff4078d454e2ddb9bb1b22
Greg <greg.renfro93@gmail.com>
2026-09-10 18:37:51 -0700

    Commit S3 backend configuration

--- providers.tf
+++ providers.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket       = "tfstate-838207858325-us-west-2"
+    key          = "aws_vpc/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 
 
